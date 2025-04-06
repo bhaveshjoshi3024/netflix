1366955a532c6c5af4002ac2037947d5798a86d2
bhaveshjoshi3024 <bhaveshjoshi3024@gmail.com>
2025-04-06 12:30:00 +0530

    added the env for security purpose

--- src/config.js
+++ src/config.js
@@ -1,19 +1,11 @@
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAua-SH2zDl_4xPIp5oM7tQyGdR_0FbmIk",
-  authDomain: "netflix-clone-50b33.firebaseapp.com",
-  projectId: "netflix-clone-50b33",
-  storageBucket: "netflix-clone-50b33.firebasestorage.app",
-  messagingSenderId: "232007015916",
-  appId: "1:232007015916:web:56dc1f082d016a1f4174c9"
+
+export const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-
-
-// craete a/c on themoviedb.org and replace access token below
-
-
-const TMDB_Access_Key="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1OWE1ZjBiYzMwYmMxN2YzMjI4ZjllNWE5NDllYzBkNSIsIm5iZiI6MTc0Mzg1NDExMi44ODgsInN1YiI6IjY3ZjExYTIwZjVhODBhYTU0NTk5OTQ5OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.tIKoEyy9oT7eV2V0Bw8h8-D7NAh5H0FW3tdS4iHYiCM";
-
-
-  export {firebaseConfig, TMDB_Access_Key};
+export const TMDB_Access_Key = import.meta.env.VITE_TMDB_ACCESS_KEY;
